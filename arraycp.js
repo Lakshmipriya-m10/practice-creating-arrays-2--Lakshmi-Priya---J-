fc89a53bae8d232800f9c3ce083b03969eef97d9
lakshmi priya <lakshmipriyaark@gmail.com>
2025-10-09 10:01:58 -0500

    array creating file added

---
+++ arraycp.js
@@ -0,0 +1,15 @@
+// Task 1: Initialize the Array
+let greeting = Array(5);
+greeting.fill("Hello");
+console.log(greeting);
+
+//Task 2: Update the Array
+greetingUpdate = greeting.fill ("Hi",3,5);
+console.log(greetingUpdate);
+
+//Task 3: Populate the Array with a for Loop
+let numbers = [5];
+for (let i = 0; i < 5; i++) {
+  numbers[i] = i * 10;
+}
+console.log(numbers);
